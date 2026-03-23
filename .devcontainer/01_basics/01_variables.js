006491303223493c9796f19ad43ce814cf939864
Shreshth Mittal <shreshth.mittal.9.i.sdpsmzn@gmail.com>
2026-03-23 09:52:48 +0000

    commit changes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 28372
+ownerName = "shreshth"
+let accountEmail = "shreshth@gmail.com"
+var accountPassword = "15255"
+console.table([ownerName,accountId,accountEmail,accountPassword])
---
+++ .devcontainer/01_basics/02_dataType.js
@@ -0,0 +1,11 @@
+"use strict";
+// console.log(3+3)
+// console.log("shreshth")
+let name = "shreshth"
+let age = 19
+let value = true
+let a = null
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof value)
+console.log(typeof a)
