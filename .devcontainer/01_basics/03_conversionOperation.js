b1c5815e4755d9652b0d4cc86ae3643d9d40c028
Shreshth Mittal <shreshth.mittal.9.i.sdpsmzn@gmail.com>
2026-03-23 10:39:33 +0000

    commit changes

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,11 +1,25 @@
-let a = "8"
-let e = "shre"
-let b =  3
-let c =  2
-let d = Number(a)
-let f = Number(e)
-console.log(typeof a,typeof b,typeof c,typeof d)
-console.log(f)
-let x = 1
-let y = Boolean(x)
-console.log(y)
+//************* Conversion *************************
+
+// let a = "8"
+// let e = "shre"
+// let b =  3
+// let c =  2
+// let d = Number(a)
+// let f = Number(e)
+// console.log(typeof a,typeof b,typeof c,typeof d)
+// console.log(f)
+// let x = 1
+// let y = Boolean(x)
+// console.log(y)
+
+//************************** Operations *****************************
+
+
+// let value =  3
+// let negativeVAlue = -value
+// console.log(negativeVAlue)
+
+let str1 = "Shreshth"
+let str2 = " Mittal"
+let str3 = str1+str2
+console.log(str3)
