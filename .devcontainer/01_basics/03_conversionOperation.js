d8a061829370ff8d1b5779c7a7c7a5c1c78fe93c
Shreshth Mittal <shreshth.mittal.9.i.sdpsmzn@gmail.com>
2026-03-23 10:08:32 +0000

    commit changes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let a = "8"
+let e = "shre"
+let b =  3
+let c =  2
+let d = Number(a)
+let f = Number(e)
+console.log(typeof a,typeof b,typeof c,typeof d)
+console.log(f)
+let x = 1
+let y = Boolean(x)
+console.log(y)
