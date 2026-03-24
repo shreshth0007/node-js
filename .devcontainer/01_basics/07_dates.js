2848690ad69c2c56b055d037f2632e06aa1201ec
Shreshth Mittal <shreshth.mittal.9.i.sdpsmzn@gmail.com>
2026-03-24 17:52:08 +0000

    dates

---
+++ .devcontainer/01_basics/07_dates.js
@@ -0,0 +1,8 @@
+let myDate = new Date("03-23-2026")
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toJSON())
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myDate.getTime())
