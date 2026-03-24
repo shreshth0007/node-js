4e0c797ad8a3582dbfbaaac1870cc40ea79227b3
Shreshth Mittal <shreshth.mittal.9.i.sdpsmzn@gmail.com>
2026-03-24 13:57:15 +0000

    save

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// console.log(undefined>0)
+// console.log(undefined<0)
+// console.log(undefined==0)
+
+
+// console.log(3>"4")
+
+const heros = ["ironman","hulk","thor"]
+let myobj = {
+    name: "shreshth",
+    age: 22,
+}
+const myFunction = function(){
+    console.log("Hello World");
+}
---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,17 @@
+// const name = "shreshth mittal"
+// const repoCount = 50
+// // console.log(name + repoCount +" values");
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+
+// const name = "shrehth"
+// const age = 19
+// console.log(`I am ${name} and i am ${age} years old`)
+
+// const url = "https://cautious-waddle-wrj4j5vjpqqrf54j9.github.dev/"
+// console.log(url.includes('waddle'))
+// console.log(url.replace('cautious','hello'))
+// console.log(url)
+
+const gamename = new String('hitesh-hc-c')
+console.log(gamename.split('-'))
