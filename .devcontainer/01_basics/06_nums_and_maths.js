b770b36cf27949f1d550915defe85e57f555e45c
Shreshth Mittal <shreshth.mittal.9.i.sdpsmzn@gmail.com>
2026-03-24 17:28:50 +0000

    maths

---
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -0,0 +1,41 @@
+// const score = 300 
+//  const balance = new Number(200)
+//  console.log(balance)
+//  console.log(balance.toString().length.toFixed(3))
+//  const number = 100000
+//  console.log(number.toLocaleString('en-IN'))
+
+ //****************** maths **********************
+
+ 
+//  console.log(Math.abs(-4))
+//  console.log(Math.round(4.5))
+//  console.log(Math.ceil(4.5))
+//  console.log(Math.floor(4.5))
+//  console.log(Math.min(9,5,6,7))
+//  console.log(Math.random().toFixed(1)*100+Math.random().toFixed(1)*10)
+const min = 1
+const max = 100
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
