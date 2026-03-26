b319efbd31f5675b0f9ed2bac1cc485123663812
Shreshth Mittal <shreshth.mittal.9.i.sdpsmzn@gmail.com>
2026-03-26 14:33:28 +0000

    new

---
+++ .devcontainer/03_basics/02_scopes.js
@@ -0,0 +1,19 @@
+// function one() {
+//     const name = "shreshth"
+//     function two(){
+//         const website = "youtube"
+//         console.log(name)
+//     }
+//     // console.log(website)
+//    two()
+// }
+// one()
+
+function addone(num){
+    return num + 1
+}
+console.log(addone(5))
+const addtwo = function(num){
+    return num + 2
+}
+console.log(addtwo(5))
---
+++ .devcontainer/03_basics/03_arrow.js
@@ -0,0 +1,17 @@
+// const user = {
+//     username : "shreshth",
+//     price : 999,
+//     welcome : function(){
+//         console.log(`welcome ${this.username} to our website`)
+        // console.log(this)
+//     }
+// }
+// user.welcome()
+// user.username = "krishna"
+// user.welcome()
+// console.log(this)
+
+function shreshth(){
+    console.log(this) // this work in object dont 
+}
+shreshth()
