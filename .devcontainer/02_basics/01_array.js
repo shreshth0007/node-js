83dcb2995ea40a0a8f3c093e5db3ec07e9a7ff75
Shreshth Mittal <shreshth.mittal.9.i.sdpsmzn@gmail.com>
2026-03-26 05:48:23 +0000

    array

---
+++ .devcontainer/02_basics/01_array.js
@@ -0,0 +1,21 @@
+// const arr = [1,2,3,5]
+// console.log(arr[3])
+// console.log(arr.length)
+// console.log(arr)
+// arr.push(6)
+// arr.push(6)
+// arr.pop()
+// arr.push(6)
+// console.log(arr)
+// arr.unshift(9) // add 9 at beginning
+// arr.shift() // remove or shift all elements left side
+// console.log(arr)
+// console.log(arr.includes(5))
+// console.log(arr.indexOf(3))
+
+const arr = [1,2,3,5]
+// const newArr = arr.join()
+// console.log(arr)
+// console.log(newArr)
+console.log("A ",arr)
+console.log("B ",arr.slice(1,3))
---
+++ .devcontainer/02_basics/02_array.js
@@ -0,0 +1,19 @@
+// const marvel_heros = ["ironman","Thor","Hulk"]
+// const dc_heros = ["superman","flash","batman"]
+// // marvel_heroes.push(dc_heroes)
+// // const all_heros = marvel_heros.concat(dc_heros)
+// const all_heros = [...marvel_heros,...dc_heros]
+// console.log(all_heros)
+
+// const anotherArray = [1,2,3,[4,5,6,7],8,[3,5,6,[7,9]]]
+// const real_anotherArray = anotherArray.flat(Infinity)
+// console.log(real_anotherArray)
+
+// console.log(Array.isArray("Shreshth"))
+// console.log(Array.from("Shreshth"))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
